52be1432fa7d398947ec162c8a6f507ca4891b95
Carvell Scott <carvell.scott@gmail.com>
2023-08-04 17:13:33 -0500

    feature: added license and additional polishing

--- eval_lhs/__init__.py
+++ eval_lhs/__init__.py
@@ -5,6 +5,7 @@ from inspect import currentframe, getframeinfo
 import uuid
 import argparse
 import ast
+import functools
 import sys
 import code
 
@@ -65,9 +66,38 @@ class Replacer(ast.NodeTransformer):
         return node
 
 
-def _run_replacement(replacement, prev_globals, prev_locals):
+def _run_replacement(prev_globals, prev_locals, replacement):
     code_obj = code.compile_command(replacement, symbol="eval")
     return eval(code_obj, prev_globals, prev_locals)
+
+
+class AssertionTransformer(ast.NodeTransformer):
+    """
+    This class will replace the right-hand-side of an assertion with a
+    previously-calculated value
+    """
+    def __init__(self, replacer_func):
+        self._replacer_func = replacer_func
+        self._replacement_made = None
+
+    def visit_Assert(self, node):
+        if not hasattr(node.test, "left"):
+            return node
+        if not hasattr(node.test, "comparators"):
+            return node
+        for i, comparator in enumerate(node.test.comparators):
+            if not isinstance(comparator, ast.Name):
+                continue
+            # If the comparator id matches the module, we've found the target
+            # to replace
+            if comparator.id == __name__:
+                raw_lhs = ast.unparse(ast.Expr(node.test.left))
+                value = self._replacer_func(raw_lhs)
+
+                node.test.comparators[i] = ast.Constant(value)
+                #self._replacement_made = ast.unparse(node)
+                #print(ast.dump(node, indent=4))
+        return node
 
 
 class _EvalLHS:
@@ -79,24 +109,27 @@ class _EvalLHS:
     It is preferred that you use the module name in place of this class for
     convenience.
     """
+    def __init__(self):
+        self.rewritten_output = None
+
     def __eq__(self, other):
         caller_frame = getframeinfo(currentframe().f_back)
         calling_filename = caller_frame.filename
         calling_lineno = caller_frame.lineno # calling line is 1 based
         prev_globals = currentframe().f_back.f_globals
         prev_locals = currentframe().f_back.f_locals
+        replacer_func = functools.partial(_run_replacement, prev_globals, prev_locals)
         searcher = Searcher(calling_lineno)
+        transformer = AssertionTransformer(replacer_func)
         with open(calling_filename) as f:
             source = f.read()
             # Parse the syntax tree of the source.
             tree = ast.parse(source)
             # Populate searcher with the location of this instance of Snapshot.
-            post_visit_tree = searcher.visit(tree)
-            assert tree is post_visit_tree
+            post_visit_tree = transformer.visit(tree)
+
 
             # Evaluate the left-hand-side of the "==" for this instance
-            replacement = _run_replacement(searcher.raw_lhs, prev_globals,
-                                           prev_locals)
 
             # Fun trick for your .vimrc:
             # iabbrev eval_lhs eval_lhs<Esc>:w<CR>7Xx:.r !./%<CR>kJ$
@@ -111,11 +144,17 @@ class _EvalLHS:
             lines = source.splitlines()
             tree = ast.parse(source)
             #print(ast.dump(tree, indent=4))
-            replacer = Replacer(replacement, calling_lineno)
-            final_tree = ast.fix_missing_locations(replacer.visit(tree))
-            lines[calling_lineno - 1] = replacer._replacement_made
-            print("\n".join(lines))
+            #replacer = Replacer(replacement, calling_lineno)
+            final_tree = ast.fix_missing_locations(post_visit_tree)
+            self.rewritten_output = ast.unparse(final_tree)
+            #lines[calling_lineno - 1] = replacer._replacement_made
+            #print("\n".join(lines))
         return True
+
+    def write(self, filepath):
+        with open(filepath, "w") as f:
+            print(self.rewritten_output, file=f)
+
 
 
 sys.modules[__name__] = _EvalLHS()
